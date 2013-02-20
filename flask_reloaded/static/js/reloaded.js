4cc450ad4da8293bc9d0f5ac9ba0f1d023007362
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2013-02-20 13:21:35 -0500

    some attempts at making it work in IE... dubious

--- flask_reloaded/static/js/reloaded.js
+++ flask_reloaded/static/js/reloaded.js
@@ -1,4 +1,4 @@
-;(function($){
+;(function(JSON, $){
   "use strict";
 
   /*
@@ -12,75 +12,107 @@
     var api = {},
       _url = "/reloaded/",
       _interval = 1.0,
-      _interval_id = null,
       _stopped = true,
+      _waiting = true,
       _mtime = null,
       _panel = false,
-      _enabled = false;
+      _enabled = false,
+      _cookie_fields = ["enabled", "interval"];
 
     api.init = function(){
       if(api.panel()){
-        return api.init_panel();
+        // this might do some cookie stuff
+        api.init_panel();
+      }else{
+        api.enabled(true);
       }
       
-      return api.start();
+      if(api.enabled()){
+        return api.start();  
+      }
+      
+      return api;
     };
     
-    api.cookie_up = function(){
-      // if there is jquery, add cookie
+    api.init_cookie = function(){
+      /* if there is jquery, add cookie
+       * once https://github.com/mgood/flask-debugtoolbar/pull/42 hits, this 
+       * will be unnecessary
+       * 
+       */
       if($){
         $.cookie = function(name, value, options) { if (typeof value != 'undefined') { options = options || {}; if (value === null) { value = ''; options.expires = -1; } var expires = ''; if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) { var date; if (typeof options.expires == 'number') { date = new Date(); date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000)); } else { date = options.expires; } expires = '; expires=' + date.toUTCString(); } var path = options.path ? '; path=' + (options.path) : ''; var domain = options.domain ? '; domain=' + (options.domain) : ''; var secure = options.secure ? '; secure' : ''; document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join(''); } else { var cookieValue = null; if (document.cookie && document.cookie != '') { var cookies = document.cookie.split(';'); for (var i = 0; i < cookies.length; i++) { var cookie = $.trim(cookies[i]); if (cookie.substring(0, name.length + 1) == (name + '=')) { cookieValue = decodeURIComponent(cookie.substring(name.length + 1)); break; } } } return cookieValue; } };
+      }else{
+        return;
       }
       
-      ["enabled", "duration"].map(function(to_wrap){
-        var old = api[to_wrap],
-          cname = "fldt-reloaded-" + to_wrap;
-          
-        try{
-          old(JSON.parse($.cookie(cname)).val);
-        }catch(err){
-          // probably just the wind
-        }
-        
+      var current_values = api.cookie();
+            
+      $.each(_cookie_fields, function(idx, to_wrap){
+        var old = api[to_wrap];
+        old(current_values[to_wrap]);
         api[to_wrap] = function(val){
           if(!arguments.length){return old();}
           
-          $.cookie(cname, JSON.stringify({val: val}));
+          api.cookie(to_wrap, val);
           return old(val);
         };
       });
     };
     
+    api.cookie = function(key, val){
+      var _cookie_name = "fldt-reloaded",
+        current_values = {};
+        
+      $.each(_cookie_fields, function(idx, key){
+          current_values[key] = api[key]();
+      });
+      
+      try{
+        $.extend(current_values, JSON.parse($.cookie(_cookie_name)));
+      }catch(err){
+        // probably just the wind
+      }
+      
+      if(arguments.length === 0){ return current_values; }      
+      if(arguments.length === 1){ return current_values[key]; }
+      
+      current_values[key] = val;
+      
+      $.cookie(_cookie_name, JSON.stringify(current_values));
+      
+      return api;
+    };
+    
     api.init_panel = function(){
-      api.cookie_up();
+      var form = $("#flDebug form.fdbt-reloaded");
       
-      var form = $("#flDebug form.fdbt-reloaded"),
-        subtitle = $("#flDebugAutoreloadPanel small"),
-        update_subtitle = function(){
-          subtitle.text(
-            api.enabled() ? ("Every " + api.interval() + " sec") : "Disabled"
-          );
-        };
+      api.init_cookie();
       
-      update_subtitle();
-      
-      form.find(".fdbt-reloaded-enabled")
-        .val(api.enabled())
+      var chk_enbl = form.find(".fdbt-reloaded-enabled")
         .change(function(){
-          api
-            .enabled($(this).filter(":checked").length !== 0)
+          api.enabled($(this).filter(":checked").length !== 0)
+            .panel_subtitle()
             .restart_if_enabled();
-          update_subtitle();
         });
+      
+      if(api.enabled()){chk_enbl.attr("checked", "checked");}
       
       form.find(".fdbt-reloaded-interval")
         .val(api.interval())
         .change(function(){
           api.interval($(this).val())
+            .panel_subtitle()
             .restart_if_enabled();
-          update_subtitle();
         });
       
+      return api.panel_subtitle();
+    };
+    
+    api.panel_subtitle = function(){
+      $("#flDebugAutoreloadPanel small").text(
+        api.enabled() ? ("Every " + api.interval() + " sec") : "Disabled"
+      );
       return api;
     };
     
@@ -136,23 +168,29 @@
 
       client.onreadystatechange = function() {
         if(this.readyState == this.DONE && this.status == 200) {
+          _waiting = false;
           try{
-            api.reload(JSON.parse(this.responseText).mtime)
+            api.reload(JSON.parse(this.responseText).mtime);
           }catch(err){
             // it's probably nothing...
+          }
+          if(!_stopped){
+            window.setTimeout(api.fetch, _interval * 1000);
+            _waiting = true;
           }
         }
       };
 
       client.open("GET", _url + "?r=" + Math.random());
       client.send();
-
+      
       return api;
     };
     
     api.start = function() {
       _stopped = false;
-      _interval_id = window.setInterval(api.fetch, _interval * 1000);
+      window.setTimeout(api.fetch, _interval * 1000);
+      _waiting = true;
       return api;
     };
     
@@ -166,8 +204,6 @@
 
     api.stop = function () {
       _stopped = true;
-      window.clearInterval(_interval_id);
-      _interval_id = null;
       
       return api;
     };
@@ -175,4 +211,4 @@
     return api;
   }
 
-}).call(this, window.jQuery && window.jQuery.noConflict(true));
+}).call(this, JSON, window.jQuery && window.jQuery.noConflict(true));
